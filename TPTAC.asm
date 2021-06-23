05d89e2a2fe77f65658304cf512585ca2e9cc019
founder <djjotaremix@gmail.com>
2021-06-23 14:04:22 +0100

    primeiro commit

---
+++ TPTAC.asm
@@ -0,0 +1,26 @@
+;/--------------------------------------------------\
+;|	             TRABALHO PRATICO DE                |
+;|    TECNOLOGIAS E ARQUITETURAS DE COMPUTADORES    |
+;|	           ANO LETIVO DE 2020/2021              |
+;<-------------------------------------------------->
+;|          João André Linhares Oliveira            |
+;|                   2018012875                     |
+;|         Pedro Miguel Pinheiro Miranda            |
+;|                   2020139811                     |
+;\--------------------------------------------------/
+
+; START
+.8086
+.model small
+.stack 2048
+
+; CODE
+
+
+
+; MAIN
+Main    proc
+
+Main    endp
+Cseg	ends
+END     Main
